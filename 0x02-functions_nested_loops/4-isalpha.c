6df81828da0567e9b9fb8d5f83256713c9ec3aab
abidemy <ogunnowomichaelabidemi@gmail.com>
2022-09-14 00:54:22 +0100

    Nested Loop

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 /**
  * _isalpha - Check Holberton
- * c: An input character
+ * @c: An input character
  * Description: function returns 1 if the character is a
  * letter, lowercase or uppercase.
  * Return: 1 or 0 in otherwise.
