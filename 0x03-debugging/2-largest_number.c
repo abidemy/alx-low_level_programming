10c44f29c4a2df0cd35a360cbf10352702e6846b
abidemy <ogunnowomichaelabidemi@gmail.com>
2022-09-16 02:18:39 +0100

    972 is largest

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *largest_number - returns the largest number of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number of 3 numbers
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a > b && b > c)
+	{
+		largest = a;
+	}
+
+	else if (b > a && a > c)
+	{largest = b;
+
+	}
+
+	else if (a > b && c > b)
+	{largest = a;}
+	
+	else
+	{
+		largest = c;
+	}
+
+	return (largest);
+}
