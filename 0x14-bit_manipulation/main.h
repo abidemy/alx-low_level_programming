33684d0081139821746037d9c915dec9eac047b6
abidemy <ogunnowomichaelabidemi@gmail.com>
2022-10-28 03:14:00 +0100

    bit manipulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,6 +5,9 @@
  * Desc: Header file containing prototypes for all functions
  * written in the 0x13-bit_manipulation directory
  */
+#include <stddef.h>
+
+#include <stdio.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
